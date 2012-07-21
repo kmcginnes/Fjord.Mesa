fe9f8e1cb5b454d26767dc7d89f2fef59fc8c9f2
Kris McGinnes <kmcginnes@me.com>
2012-07-21 13:27:37 -0500

    Added bootstrap in less form, bundle transform package, and redesigned layout to use bootstrap and new bundles

--- src/Fjord.Mesa.Web/App_Start/BundleConfig.cs
+++ src/Fjord.Mesa.Web/App_Start/BundleConfig.cs
@@ -1,5 +1,6 @@
-﻿using System.Web;
+﻿using System;
 using System.Web.Optimization;
+using BundleTransformer.Core.Transformers;
 
 namespace Fjord.Mesa
 {
@@ -7,38 +8,111 @@ namespace Fjord.Mesa
     {
         public static void RegisterBundles(BundleCollection bundles)
         {
-            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-1.*",
-                        "~/Scripts/jquery.pjax.js"));
+            bundles.AddScriptBundle("~/bundles/scripts/common")
+                .Include(
+                    "~/Scripts/jquery-{version}.js",
+                    "~/Scripts/jquery.pjax.js",
+                    "~/Scripts/jquery.unobtrusive*",
+                    "~/Scripts/jquery.validate*");
 
-            bundles.Add(new ScriptBundle("~/bundles/jqueryui").Include(
-                        "~/Scripts/jquery-ui*"));
+            bundles.AddScriptBundle("~/bundles/scripts/jqueryui")
+                .Include("~/Scripts/jquery-ui*");
+            
+            bundles.AddScriptBundle("~/bundles/scripts/modernizr")
+                .Include("~/Scripts/modernizr-{version}.js");
 
-            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
-                        "~/Scripts/jquery.unobtrusive*",
-                        "~/Scripts/jquery.validate*"));
+            bundles.AddScriptBundle("~/bundles/scripts/application")
+                .IncludeDirectory("~/Scripts/Application", "*.js");
 
-            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
-                        "~/Scripts/modernizr-*"));
+            bundles.AddStyleBundle("~/bundles/styles/common")
+                .Include("~/Content/site.css")
+                .Include("~/Content/less/bootstrap.less");
 
-            bundles.Add(new ScriptBundle("~/bundles/application").IncludeDirectory(
-                        "~/Scripts/Application", "*.js"));
+            bundles.AddStyleBundle("~/bundles/styles/jqueryui")
+                .Include(
+                    "~/Content/themes/base/jquery.ui.core.css",
+                    "~/Content/themes/base/jquery.ui.resizable.css",
+                    "~/Content/themes/base/jquery.ui.selectable.css",
+                    "~/Content/themes/base/jquery.ui.accordion.css",
+                    "~/Content/themes/base/jquery.ui.autocomplete.css",
+                    "~/Content/themes/base/jquery.ui.button.css",
+                    "~/Content/themes/base/jquery.ui.dialog.css",
+                    "~/Content/themes/base/jquery.ui.slider.css",
+                    "~/Content/themes/base/jquery.ui.tabs.css",
+                    "~/Content/themes/base/jquery.ui.datepicker.css",
+                    "~/Content/themes/base/jquery.ui.progressbar.css",
+                    "~/Content/themes/base/jquery.ui.theme.css");
+        }
+    }
 
-            bundles.Add(new StyleBundle("~/Content/css").Include("~/Content/site.css"));
+    public static class BundleExtensions
+    {
+        private static readonly Lazy<JsTransformer> JsTransformer =
+            new Lazy<JsTransformer>(() => new JsTransformer());
 
-            bundles.Add(new StyleBundle("~/Content/themes/base/css").Include(
-                        "~/Content/themes/base/jquery.ui.core.css",
-                        "~/Content/themes/base/jquery.ui.resizable.css",
-                        "~/Content/themes/base/jquery.ui.selectable.css",
-                        "~/Content/themes/base/jquery.ui.accordion.css",
-                        "~/Content/themes/base/jquery.ui.autocomplete.css",
-                        "~/Content/themes/base/jquery.ui.button.css",
-                        "~/Content/themes/base/jquery.ui.dialog.css",
-                        "~/Content/themes/base/jquery.ui.slider.css",
-                        "~/Content/themes/base/jquery.ui.tabs.css",
-                        "~/Content/themes/base/jquery.ui.datepicker.css",
-                        "~/Content/themes/base/jquery.ui.progressbar.css",
-                        "~/Content/themes/base/jquery.ui.theme.css"));
+        private static readonly Lazy<CssTransformer> CssTransformer =
+            new Lazy<CssTransformer>(() => new CssTransformer());
+
+        public static Bundle AddBundle(this BundleCollection bundles, string virtualPath)
+        {
+            if(virtualPath.IsNullOrWhitespace())
+            {
+                throw new ArgumentNullException(virtualPath);
+            }
+
+            var bundle = new Bundle(virtualPath);
+            bundles.Add(bundle);
+            return bundle;
+        }
+
+        public static Bundle AddScriptBundle(this BundleCollection bundles, string virtualPath)
+        {
+            if (virtualPath.IsNullOrWhitespace())
+            {
+                throw new ArgumentNullException(virtualPath);
+            }
+
+            var bundle = new Bundle(virtualPath)
+                .AddTransform(JsTransformer.Value);
+            bundles.Add(bundle);
+            return bundle;
+        }
+
+        public static Bundle AddStyleBundle(this BundleCollection bundles, string virtualPath)
+        {
+            if (virtualPath.IsNullOrWhitespace())
+            {
+                throw new ArgumentNullException(virtualPath);
+            }
+
+            var bundle = new Bundle(virtualPath)
+                .AddTransform(CssTransformer.Value);
+            bundles.Add(bundle);
+            return bundle;
+        }
+
+        public static Bundle AddTransform(this Bundle bundle, params IBundleTransform[] transforms)
+        {
+            if(transforms == null)
+            {
+                throw new ArgumentNullException("transforms");
+            }
+
+            foreach (var transform in transforms)
+            {
+                bundle.Transforms.Add(transform);
+            }
+            return bundle;
+        }
+
+        public static void AddTo(this Bundle bundle, BundleCollection bundleCollection)
+        {
+            if(bundleCollection == null)
+            {
+                throw new ArgumentNullException("bundleCollection");
+            }
+
+            bundleCollection.Add(bundle);
         }
     }
 }
--- src/Fjord.Mesa.Web/Scripts/_references.js
+++ src/Fjord.Mesa.Web/Scripts/_references.js
@@ -5,3 +5,4 @@
 /// <reference path="jquery.validate.unobtrusive.js" />
 /// <reference path="knockout-2.1.0.debug.js" />
 /// <reference path="modernizr-2.5.3.js" />
+/// <reference path="bootstrap/bootstrap.js" />
